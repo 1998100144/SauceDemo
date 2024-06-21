09d8afc971e518c03b2c0bec14ff17541c5cf147
Kasimova Zarina <zosyas14@gmail.com>
2024-06-22 00:08:34 +0400

    Initial commit

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -6,47 +6,21 @@ import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
-// id
-//name
-//classname
-//tagname
-//linktext
-//partiallinktext
-//xpath
-//Поиск по атрибуту, например By.xpath("//tag[@attribute='value']");
-//Поиск по тексту, например By.xpath("//tag[text()='text']");
-//Поиск по частичному совпадению атрибута, например By.xpath("//tag[contains(@attribute,'text')]");
-//Поиск по частичному совпадению текста, например By.xpath("//tag[contains(text(),'text')]");
-//ancestor, например //*[text()='Enterprise Testing']//ancestor::div
-//descendant
-//following
-//parent
-//preceding
-//Подсказка:  XPath Axes
-//*поиск элемента с условием AND, например //input[@class='_2zrpKA _1dBPDZ' and @type='text']
-//css
-//.class
-//.class1.class2
-//.class1 .class2
-//#id
-//tagname
-//tagname.class
-//[attribute=value]
+
 
 public class NewTest {
 
    @Test
     public void swagLabs1() {
-        // открыть браузер
-        //зайти на сайт
+
        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
        WebDriver browser = new ChromeDriver();
        browser.get("https://www.saucedemo.com");
        browser.findElement(By.id("user-name"));
        browser.findElement(By.name("user-name"));
        browser.findElement(By.className("submit-button"));
-       //browser.findElement(By.linkText(""));
-       //browser.findElement(By.partialLinkText("pricing"));
+       //browser.findElement(By.linkText("text")); не нашла подходящий пример с тегом <a>
+       //browser.findElement(By.partialLinkText("text")); не нашла подходящий пример с тегом <a>
        browser.findElement(By.xpath("//input[@id='user-name']"));
        browser.findElement(By.xpath("//h4[text()='Accepted usernames are:']"));
        browser.findElement(By.xpath("//input[contains(@placeholder,'Password')]"));
